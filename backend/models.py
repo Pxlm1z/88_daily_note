10e3d7842ecf8d1528c341824b37609d32503f69
Pxlm1z <palmhver01@gmail.com>
2025-06-18 11:44:54 +0700

    update comment on model

--- backend/models.py
+++ backend/models.py
@@ -3,11 +3,11 @@ from mongoengine import Document, StringField, ReferenceField, DateTimeField
 from datetime import datetime
 
 class User(Document):
-    username = StringField(required=True, unique=True)
-    password = StringField(required=True)
+    username = StringField(required=True, unique=True)#ชื่อผู้ใช้
+    password = StringField(required=True)#รหัสผ่าน
 
 class Note(Document):
-    title = StringField(required=True)
-    content = StringField()
-    created_at = DateTimeField(default=datetime.utcnow)
+    title = StringField(required=True)#ชื่อเรื่อง
+    content = StringField()#เนื้อหา
+    created_at = DateTimeField(default=datetime.utcnow)#วันที่สร้าง
     user = ReferenceField(User)
